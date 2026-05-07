1bcddb4fc168073cfe52ac9ca0e78ee4140d8652
smirihamza0008-glitch <smirihamza0008@gmail.com>
2026-05-07 19:11:53 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/planets_explorer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/planets_explorer/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.planets_explorer
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // إضافة هذا الجزء يضمن تعريف النشاط بشكل صحيح للنظام
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
